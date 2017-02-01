1a21d48d004656ee7262a926c45806c402f39594
Ryan Muskopf <ryanjmuskopf@gmail.com>
2017-02-01 02:54:26 -0500

    first upload

---
+++ src/imageResponses.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const dankMemes = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getDankMemes = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(dankMemes);
+  response.end();
+};
+module.exports.getDankMemes = getDankMemes;
